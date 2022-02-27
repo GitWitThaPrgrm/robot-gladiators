0498f35e47dbcebce36256c6f05426ac9ca0ef36
BLO <pablomiske@gmail.com>
2022-02-26 21:16:16 -0800

    add player name

---
+++ assets/js/game.js
@@ -0,0 +1,7 @@
+var playerName = window.prompt("what is your robot's name?");
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+console.log("Your robot, " + playerName + ", has won");
